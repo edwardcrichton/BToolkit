0ffbe636abd3309fc464b1ab3243e5c9914ba5ae
Piotr Trojanek <piotr.trojanek@gmail.com>
2014-05-28 20:52:34 +0100

    extra space removed

--- MOTIF/B_Motif.c
+++ MOTIF/B_Motif.c
@@ -11425,7 +11425,7 @@ fflush ( stdout );
   get parent dialog/OK or CANCEL/file_diff_but/rad_but
   ***/
   if ( cbs->reason == XmCR_PROTOCOLS ) {    /*** WM Close ***/
-    dialog =  button;
+    dialog = button;
     ok_pressed = 0;
     file_diff_but = 999;
 /***
